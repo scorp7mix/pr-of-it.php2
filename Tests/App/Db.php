0cdd819fc7eafe79a07e3415d74a9caa8a614026
scorp7mix <scorp7mix@gmail.com>
2016-01-28 21:47:35 +0300

    fix tests/db

--- Tests/App/Db.php
+++ Tests/App/Db.php
@@ -22,11 +22,11 @@ echo check(
  * Check query method if it returns non zero array (row with id should exists in database table)
  */
 echo check(
-    $db->query("SELECT * FROM users WHERE id = 1", \App\Models\User::class),
+    $db->query("SELECT * FROM users WHERE id = 1", '\App\Models\User'),
     'Db->query method without data params'
 );
 echo check(
-    $db->query("SELECT * FROM users WHERE id = :id", \App\Models\User::class, ['id' => 1]),
+    $db->query("SELECT * FROM users WHERE id = :id", '\App\Models\User', ['id' => 1]),
     'Db->query method with data params'
 );
 
